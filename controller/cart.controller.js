16c8c454ae1d407083480d9d9bd20cd86c329673
LSAfrika <lstudiosafrika@gmail.com>
2023-08-18 09:04:38 +0300

    finished cart logic on post route

--- controller/cart.controller.js
+++ controller/cart.controller.js
@@ -1,11 +1,13 @@
 const{cartmodel}=require('../models/cart.model')
+const{productmodel}=require('../models/products.model')
 
 
 exports.getcart=async(req,res)=>{
     try {
         const{userid}=req.body
-        const usercart= await cartmodel.findById(userid)
-        if(usercart==null)res.send({exceptionmessage:'no cart found'})
+        console.log('user id',userid);
+        let usercart= await cartmodel.findById(userid)
+        if(usercart==null)  usercart=await cartmodel.create({_id:userid,products:[]})
         res.send({cart:usercart})
         
     } catch (error) {
@@ -16,9 +18,70 @@ exports.getcart=async(req,res)=>{
 }
 exports.addtocart=async(req,res)=>{
     try {
-        const{userid,product}=req.body
+        const{userid,cartproducts}=req.body
 
-        console.log(product);
+        let localcartproducts=[]
+        // let cart = await cartmodel.findById(userid)
+        // if(cart==null)usercart=await cartmodel.create({_id:userid,products:[]})
+
+
+        // console.log(cartproducts);
+
+
+   cartproducts.forEach(async(_product) => {
+
+            // console.log('foreach loop working',_product)
+            const fetchedproduct= await productmodel.findById(_product.product.id)
+            if(fetchedproduct !=null){
+                // console.log('fetched product',fetchedproduct);
+                producttosave={
+                    product:fetchedproduct.id,
+                    quantity:_product.product.quantity,
+                    sumtotal:fetchedproduct.productprice *_product.product.quantity
+                }
+                localcartproducts.push(producttosave)
+
+
+                
+
+                if(cartproducts.length==localcartproducts.length){
+                    
+                    const totalproductsprice= localcartproducts.map(p=>p.sumtotal).reduce(sumofArray)
+                    console.log(totalproductsprice);
+
+
+                    let usercart= await cartmodel.findById(userid)
+                    if(usercart==null){
+            
+                        usercart=await cartmodel.create({
+                            _id:userid,
+                            products:localcartproducts,
+                            totalprice:totalproductsprice
+            
+                        })
+
+                    return res.send({message:'cart created successfully',usercart});
+                     
+                    }
+                    if(usercart.products.length==0){
+
+                        usercart.products=[...localcartproducts]
+                        usercart.totalprice=totalproductsprice
+                        await usercart.save()
+
+
+                        return res.send({message:'cart updated successfully',usercart});
+                    }
+
+                
+                }
+            }
+
+            
+        });
+
+     
+
         return
         let usercart= await cartmodel.findById(userid)
         if(usercart==null){
@@ -70,4 +133,8 @@ exports.checkoutcart=async(req,res)=>{
         res.send({errormessage:error.message,error})
         
     }   
+}
+
+function sumofArray(sum, num) {
+    return sum + num;
 }
--- models/cart.model.js
+++ models/cart.model.js
@@ -5,11 +5,22 @@ const mongoose= require('mongoose')
 
 exports.cartmodel = mongoose.model('cart',mongoose.Schema({
 
-    cartowner:{type:mongoose.Schema.Types.ObjectId,required:true,ref:'user'},
-    products:[{type:mongoose.Schema.Types.ObjectId,required:true,ref:'product'}],
+    // cartowner:{type:mongoose.Schema.Types.ObjectId,required:true,ref:'user'},
+    products:[
+        {product:{type:mongoose.Schema.Types.ObjectId,required:true,ref:'product'},
+        quantity:{ type:Number,required:true,default:1 },
+        sumtotal:{type:Number,required:true,default:0}
+        }
+            ],
     totalprice:{type:Number,required:true,default:0},
     cartcheckedout:{type:Boolean,required:true,default:false}
 
 },{timestamps:true}
 
 ))
+
+function totalproductprice(productprice,quantity){
+
+    return productprice * quantity
+
+}
