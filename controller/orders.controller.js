2f85fc5397a40c42d8e7ccce0c0bc0edd9245468
LSAfrika <lstudiosafrika@gmail.com>
2023-12-28 20:23:57 +0300

    finished specific order status update

--- controller/orders.controller.js
+++ controller/orders.controller.js
@@ -20,24 +20,7 @@ exports.storeorders=async(req,res)=>{
     }
 }
 
-exports.completeorder=async(req,res)=>{
-    try {
 
-        const {userid}=req.body
-
-        const store= await storemodel.findOne({storeowner:userid})
-
-        if(store==null) return res.status(404).send({exceptionmessage:'store not found'})
-
-        const storeorder= await createordermodel.findOne({storeid:store._id})
-
-        res.send(storeorder)
-        
-    } catch (error) {
-        console.log('error store order controller: ',error.message);
-        res.send({errormessage:error.message})
-    }
-}
 
 
 exports.storeorder =async(req,res)=>{
@@ -54,6 +37,29 @@ exports.storeorder =async(req,res)=>{
         if (storeorder==null) return res.status(404).send({exceptionmessage:'order not found'})
             
         
+        res.send(storeorder)
+        
+    } catch (error) {
+        console.log('error store order controller: ',error.message);
+        res.send({errormessage:error.message})
+    }
+}
+
+exports.completeorder =async(req,res)=>{
+    try {
+
+        const {userid}=req.body
+        const{orderid}=req.params
+        const store= await storemodel.findOne({storeowner:userid})
+
+        if(store==null) return res.status(404).send({exceptionmessage:'store not found'})
+
+        const storeorder= await createordermodel.findOne({storeid:store._id,_id:orderid})
+
+        if (storeorder==null) return res.status(404).send({exceptionmessage:'order not found'})
+        if (storeorder.orderstatus=='completed') return res.send({message:'order already proccessed'})    
+        storeorder.orderstatus='completed'
+        await storeorder.save()
         res.send(storeorder)
         
     } catch (error) {
